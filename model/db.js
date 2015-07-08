a10cecb46e33886fd3176db98705b044e9de80a9
NathanMBP <nathanstaylor1@gmail.com>
2015-07-08 11:26:02 +1000

    deploy MongoLab

--- model/db.js
+++ model/db.js
@@ -1,2 +1,2 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/espresso');
+mongoose.connect('mongodb://liveuser:R7DjY75884a9nO@ds055852.mongolab.com:55852/heroku_hxhn6p18');
